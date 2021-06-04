e1a4869702168f574ef44fc1437ff304964bb647
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-04 17:32:08 -0400

    year born displayed logic

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ function App() {
 	const [usersList, setUsersList] = useState([]);
 
 	const addUserHandler = (userName, userAge) => {
+		const currentYear = new Date().getFullYear();
+		console.log(currentYear);
 		setUsersList((prevUsersList) => {
 			return [
 				...prevUsersList,
-				{ name: userName, age: userAge, id: Math.random().toString() },
+				{
+					name: userName,
+					age: userAge,
+					bornIn: currentYear - userAge,
+					id: Math.random().toString(),
+				},
 			];
 		});
 	};
