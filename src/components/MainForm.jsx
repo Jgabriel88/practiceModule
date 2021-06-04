9dbd9b3347082f004e0aa83c4908abd807d90c75
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-04 15:25:42 -0400

    main form component created

---
+++ src/components/MainForm.jsx
@@ -0,0 +1,13 @@
+const MainForm = () => {
+	return (
+		<form>
+			<label>Username</label>
+			<input type="text"></input>
+			<label>Age (Years) </label>
+			<input type="number"></input>
+			<button>Add User</button>
+		</form>
+	);
+};
+
+export default MainForm;
