d2a6be4c5d10961b22273a4673e29728db345fda
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-04 16:58:45 -0400

    created basic ErrorModal component

---
+++ src/components/UI/ErrorModal.jsx
@@ -0,0 +1,17 @@
+import Button from './Button';
+import Card from './Card';
+
+const ErrorModal = (props) => {
+	return (
+		<Card>
+			<header>
+				<h2></h2>
+			</header>
+			<div></div>
+			<footer>
+				<Button />
+			</footer>
+		</Card>
+	);
+};
+export default ErrorModal;
