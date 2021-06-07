0a46f3c32ef34061243dcfd1eb6fdb87886cc805
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-07 16:24:49 -0400

    setting up components on ErrorModal to user portal

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,24 +1,28 @@
-import Wrapper from '../Helpers/Wrapper';
 import Button from './Button';
 import Card from './Card';
 import styles from './ErrorModal.module.css';
 
-const ErrorModal = (props) => {
+const Backdrop = () => {
+	return <div className={styles.backdrop} onClick={props.onConfirm}></div>;
+};
+
+const ModalOverlay = (props) => {
 	return (
-		<Wrapper>
-			<div className={styles.backdrop} onClick={props.onConfirm}></div>
-			<Card className={styles.modal}>
-				<header className={styles.header}>
-					<h2>{props.title}</h2>
-				</header>
-				<div className={styles.content}>
-					<p>{props.message}</p>
-				</div>
-				<footer className={styles.actions}>
-					<Button onClick={props.onConfirm}>OK!</Button>
-				</footer>
-			</Card>
-		</Wrapper>
+		<Card className={styles.modal}>
+			<header className={styles.header}>
+				<h2>{props.title}</h2>
+			</header>
+			<div className={styles.content}>
+				<p>{props.message}</p>
+			</div>
+			<footer className={styles.actions}>
+				<Button onClick={props.onConfirm}>OK!</Button>
+			</footer>
+		</Card>
 	);
+};
+
+const ErrorModal = (props) => {
+	return <>{}</>;
 };
 export default ErrorModal;
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Wrapper from '../Helpers/Wrapper';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
@@ -44,7 +43,7 @@ const AddUser = (props) => {
 	};
 
 	return (
-		<Wrapper>
+		<>
 			{error && (
 				<ErrorModal
 					title={error.title}
@@ -68,7 +67,7 @@ const AddUser = (props) => {
 					<Button type="submit">Add User</Button>
 				</form>
 			</Card>
-		</Wrapper>
+		</>
 	);
 };
 
